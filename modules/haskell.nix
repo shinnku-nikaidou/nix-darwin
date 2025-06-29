2408dd87dae79ba6c854570f99a4bbc163d267bc
shinnku <nikaidou@shinnku.com>
2025-06-29 13:25:31 +0800

    add more haskell packages

--- modules/haskell.nix
+++ modules/haskell.nix
@@ -5,12 +5,77 @@ let
 
   myPackages = hp.ghcWithPackages (
     hs: with hs; [
+      # Core data structures and text processing
       text
       bytestring
       containers
       vector
       unordered-containers
+
+      # JSON and serialization
       aeson
+      aeson-pretty
+      yaml
+      binary
+      cereal
+
+      # HTTP and networking
+      http-client
+      http-client-tls
+      http-types
+      servant
+      servant-server
+      servant-client
+      warp
+      wreq
+
+      # Parsing and regex
+      parsec
+      megaparsec
+      attoparsec
+      regex-pcre
+
+      # Concurrency and parallelism
+      async
+      stm
+      parallel
+
+      # File system and IO
+      directory
+      filepath
+      temporary
+      unix
+
+      # Testing
+      hspec
+      QuickCheck
+      tasty
+      tasty-hspec
+      tasty-quickcheck
+
+      # Utility libraries
+      mtl
+      transformers
+      lens
+      optparse-applicative
+      time
+      random
+      split
+      extra
+      safe
+
+      # Database
+      sqlite-simple
+      postgresql-simple
+
+      # Logging and debugging
+      monad-logger
+      pretty-simple
+
+      # Streaming
+      conduit
+      pipes
+      streaming
     ]
   );
 
