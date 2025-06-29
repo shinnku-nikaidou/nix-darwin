23e3dd2ca8aef0562376b7058006fa2bd88ef249
shinnku <nikaidou@shinnku.com>
2025-06-29 22:22:47 +0800

    add c++ enviroment

--- modules/cpp.nix
+++ modules/cpp.nix
@@ -7,7 +7,6 @@ let
 
   # Ensure all libraries are compiled with the same clang++ 20
   gmpWithClang = pkgs.gmp.override { stdenv = clang20Stdenv; };
-  gmpxxWithClang = pkgs.gmpxx.override { stdenv = clang20Stdenv; };
   boostWithClang = pkgs.boost.override { stdenv = clang20Stdenv; };
 in
 {
@@ -17,10 +16,10 @@ in
     llvm20.clang-tools # Development tools (clangd, clang-format, clang-tidy, etc.)
     llvm20.llvm # LLVM core tools
     llvm20.lldb # Debugger
+    llvm20.libcxx # C++ standard library
 
     # Libraries compiled with unified clang++ 20
     gmpWithClang
-    gmpxxWithClang
     boostWithClang
 
     # Build tools
@@ -36,5 +35,21 @@ in
   environment.variables = {
     CC = "${llvm20.libcxxClang}/bin/clang";
     CXX = "${llvm20.libcxxClang}/bin/clang++";
+
+    # Ensure we use Nix's libc++ but system libc
+    CPLUS_INCLUDE_PATH = "${llvm20.libcxx.dev}/include/c++/v1:${gmpWithClang.dev}/include:${boostWithClang.dev}/include";
+    C_INCLUDE_PATH = "${gmpWithClang.dev}/include";
+    
+    # Add library search paths
+    LIBRARY_PATH = "${llvm20.libcxx}/lib:${gmpWithClang}/lib:${boostWithClang}/lib";
+    LD_LIBRARY_PATH = "${llvm20.libcxx}/lib:${gmpWithClang}/lib:${boostWithClang}/lib";
+
+    # pkg-config paths
+    PKG_CONFIG_PATH = "${gmpWithClang}/lib/pkgconfig:${boostWithClang}/lib/pkgconfig";
+
+    # Compiler flags to use Nix libc++ but avoid system header conflicts
+    CXXFLAGS = "-stdlib=libc++ -nostdinc++ -isystem ${llvm20.libcxx.dev}/include/c++/v1 -isystem ${gmpWithClang.dev}/include -isystem ${boostWithClang.dev}/include";
+    LDFLAGS = "-L${llvm20.libcxx}/lib -L${gmpWithClang}/lib -L${boostWithClang}/lib -lc++ -lc++abi -Wl,-rpath,${llvm20.libcxx}/lib -Wl,-rpath,${gmpWithClang}/lib -Wl,-rpath,${boostWithClang}/lib";
+
   };
 }
